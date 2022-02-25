ece74eb7a09a5b2d9ddd9a7aef29c57fa637d3de
Zheng Hongpei <zhenghongpei@gmail.com>
2022-02-25 17:37:39 +0000

    init

---
+++ components/Motor.cpp
@@ -0,0 +1,35 @@
+#include <mbed.h>
+#include <./components/Motor.h>
+#include <QEI.h>
+
+Motor::Motor(PinName my_bipolar, PinName pwm, PinName channel_a, PinName channel_b): 
+bipolar(my_bipolar), motor_pwm(pwm), 
+channel_a(channel_a), channel_b(channel_b),
+encoder(channel_a, channel_b, NC, 256) {
+    bipolar = 1;
+    PWM_PERIOD = 10;
+    motor_pwm.period_ms(PWM_PERIOD);
+    current_pulses = 0;
+    last_pulses = 0;
+}
+
+void Motor::set_power(float power) {
+    current_power = power;
+    motor_pwm.write(current_power);
+}
+
+float Motor::get_power() {
+    return current_power;
+}
+
+float Motor::get_velocity() {
+    return current_speed;
+}
+
+void Motor::measure_speed() {
+    last_pulses = current_pulses;
+    current_pulses = encoder.getPulses();
+    current_speed = (current_pulses - last_pulses) / messure_period_ms * gear_ratio;
+}
+
+
---
+++ components/Motor.h
@@ -0,0 +1,25 @@
+#include <mbed.h>
+
+class Motor {
+    private:
+        DigitalOut bipolar;
+        PwmOut motor_pwm;
+        DigitalIn channel_a;
+        DigitalIn channel_b;
+        float current_power;
+        float PWM_PERIOD;
+        Ticker ticker;
+        int current_pulses;
+        int last_pulses;
+        void measure_speed();
+        float current_speed;
+        float messure_period_ms;
+        float gear_ratio;
+        QEI encoder;
+
+    public:
+        Motor(PinName bipolar, PinName pwm, PinName channel_a, PinName channel_b);
+        float get_velocity(void);
+        void set_power(float power);
+        float get_power(void);
+}
