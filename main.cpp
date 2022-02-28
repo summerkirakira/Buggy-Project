ac587784e5877c22ee13aeafd04ead51da35b9c4
Zheng Hongpei <zhenghongpei@gmail.com>
2022-02-28 12:48:36 +0000

    Modify some pins

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <./components/DriveBoard.h>
 
 int main() {
-  DriveBoard my_drive_board(PB_6, PC_7, PC_8, PC_9, PA_8, PB_10, PA_11, PB_12, PB_4);
+  DriveBoard my_drive_board(PA_5, PA_6, PA_7, PB_6, PA_8, PB_10, PC_7, PA_9, PB_4);
   my_drive_board.set_left_motor_power(0.6);
   my_drive_board.set_right_motor_power(0.6);
   while(true);
