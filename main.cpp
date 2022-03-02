07f29c97aabfd316e91764419a1aaaf7bb504d20
Silvia <silvia@SilviadeMacBook-Air.local>
2022-03-02 07:04:10 +0000

    add feature turn around

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include <mbed.h>
 #include <./components/DriveBoard.h>
 
-typedef enum {initialisation, stop, straight, turn_left, turn_right} BuggyState;
+
+typedef enum {initialisation, stop, straight, turn_left, turn_right, turn_around} BuggyState;
 BuggyState state = initialisation;
 bool is_manul_stop = false;
 DriveBoard my_drive_board(PA_5, PA_6, PA_7, PC_10, PC_12, PB_6, PA_9,PC_7, PC_3, PC_2, PA_8);
 int turn_left_num = 0;
+int turn_right_num = 0;
+bool is_turn_left = true;
 
 int main() {
   // my_drive_board.stop_left_motor();
@@ -23,7 +26,6 @@ int main() {
     switch (state)
     {
     case initialisation:
-      // ThisThread::sleep_for(5s);
       my_drive_board.start_left_motor(base_left_power);
       my_drive_board.start_right_motor(base_right_power);
       state = straight;
@@ -34,13 +36,28 @@ int main() {
         int(100*my_drive_board.get_line_distance()), int(100*my_drive_board.get_angular()));
       if(my_drive_board.get_line_distance() >= 0.5) {
         if(turn_left_num >= 3) {
-          state = stop;
+          my_drive_board.set_check_point();
+          state = turn_around;
+          turn_left_num = 0;
           break;
         };
-        turn_left_num++;
-        state = turn_left;
-        my_drive_board.stop_left_motor();
-        my_drive_board.set_check_point();
+        if(turn_right_num >= 3) {
+          my_drive_board.set_check_point();
+          state = stop;
+          turn_right_num = 0;
+          break;
+        };
+        if(is_turn_left) {
+          turn_left_num++;
+          state = turn_left;
+          my_drive_board.stop_left_motor();
+          my_drive_board.set_check_point();
+        } else {
+          turn_right_num++;
+          state = turn_right;
+          my_drive_board.stop_right_motor();
+          my_drive_board.set_check_point();
+        }
         break;
       }
       break;
@@ -54,7 +71,31 @@ int main() {
         break;
       }
       break;
+    case turn_around:
+    printf("Turing around...Line distance: %d - angular: %d\n",
+        int(100*my_drive_board.get_line_distance()), int(100*my_drive_board.get_angular()));
+      my_drive_board.start_left_motor(0.42);
+      my_drive_board.start_right_motor(base_right_power);
+      if(my_drive_board.get_angular() > 4) {
+        state = straight;
+        is_turn_left = false;
+        my_drive_board.start_left_motor(base_left_power);
+        my_drive_board.start_right_motor(base_right_power);
+        my_drive_board.set_check_point();
+      }
+      break;
+    case turn_right:
+      printf("Turing right...Line distance: %d - angular: %d\n",
+        int(100*my_drive_board.get_line_distance()), int(100*my_drive_board.get_angular()));
+      if(my_drive_board.get_angular() <= -1) {
+        state = straight;
+        my_drive_board.set_check_point();
+        my_drive_board.start_right_motor(base_right_power);
+        break;
+      }
+      break;
     case stop:
+      printf("*********************************Buggy stopped!!!*********************************\n");
       my_drive_board.disable_all();
       break;
     default:
